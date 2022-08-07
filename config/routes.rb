32b6b91ca898102f473bc9d065556e4e03fad99a
Wesley Garcia <wes@alltradestech.com>
2022-08-07 15:08:07 -0700

    Implement functionality for request POST api/v1/sessions

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :forecast, only: %i[index]
+      resources :sessions, only: %i[create]
       resources :users, only: %i[create]
     end
   end
--- spec/requests/api/v1/sessions_request_spec.rb
+++ spec/requests/api/v1/sessions_request_spec.rb
@@ -13,6 +13,9 @@ RSpec.describe 'api/v1/sessions api endpoints', :vcr, type: :request do
         email: 'yogi@picnics.gov',
         password: 'noturaveragebear'
       }
+      user = User.create!(email: 'yogi@picnics.gov', password: 'noturaveragebear')
+      user.api_keys.create!(token: '35f291ad58d0ac2c5a6275cbe56eb4d3')
+
       post '/api/v1/sessions', headers: headers, params: JSON.generate(params)
 
       expect(response).to have_http_status(200)
@@ -23,7 +26,7 @@ RSpec.describe 'api/v1/sessions api endpoints', :vcr, type: :request do
       data = body[:data]
       expect(data.keys).to include(:id, :type, :attributes)
       expect(data.keys.count).to eq(3)
-      expect(data[:id].to_i).to eq(1)
+      expect(data[:id].to_i).to eq(user.id)
       expect(data[:type]).to eq('users')
 
       attributes = data[:attributes]
@@ -31,6 +34,62 @@ RSpec.describe 'api/v1/sessions api endpoints', :vcr, type: :request do
       expect(attributes.keys.count).to eq(2)
       expect(attributes[:email]).to eq('yogi@picnics.gov')
       expect(attributes[:api_key]).to eq('35f291ad58d0ac2c5a6275cbe56eb4d3')
+    end
+
+    describe 'SAD PATH credentials do not match records' do
+      it 'returns an error if password does not match stored records' do
+        headers = {
+          'CONTENT_TYPE' => 'application/json',
+          'ACCEPT' => 'application/json'
+        }
+        params = {
+          email: 'yogi@picnics.gov',
+          password: 'notyouraveragebear'
+        }
+        user = User.create!(email: 'yogi@picnics.gov', password: 'noturaveragebear')
+        user.api_keys.create!(token: '35f291ad58d0ac2c5a6275cbe56eb4d3')
+
+        post '/api/v1/sessions', headers: headers, params: JSON.generate(params)
+
+        expect(response).to have_http_status(401)
+
+        body = JSON.parse(response.body, symbolize_names: true)
+        expect(body.keys).to include(:data)
+
+        data = body[:data]
+        expect(data.keys).to include(:id, :type, :message)
+        expect(data.keys.count).to eq(3)
+        expect(data[:id]).to eq(nil)
+        expect(data[:type]).to eq('error')
+        expect(data[:message]).to eq('Provided credentials are invalid')
+      end
+
+      it 'returns an error if email does not match stored records' do
+        headers = {
+          'CONTENT_TYPE' => 'application/json',
+          'ACCEPT' => 'application/json'
+        }
+        params = {
+          email: 'yogi@picnics.net',
+          password: 'noturaveragebear'
+        }
+        user = User.create!(email: 'yogi@picnics.gov', password: 'noturaveragebear')
+        user.api_keys.create!(token: '35f291ad58d0ac2c5a6275cbe56eb4d3')
+
+        post '/api/v1/sessions', headers: headers, params: JSON.generate(params)
+
+        expect(response).to have_http_status(401)
+
+        body = JSON.parse(response.body, symbolize_names: true)
+        expect(body.keys).to include(:data)
+
+        data = body[:data]
+        expect(data.keys).to include(:id, :type, :message)
+        expect(data.keys.count).to eq(3)
+        expect(data[:id]).to eq(nil)
+        expect(data[:type]).to eq('error')
+        expect(data[:message]).to eq('Provided credentials are invalid')
+      end
     end
   end
 end
