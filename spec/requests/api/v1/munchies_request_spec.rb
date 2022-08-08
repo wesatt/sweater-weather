9c6a8791130ec031035b85b3fcd85143a1d14046
Wesley Garcia <wes@alltradestech.com>
2022-08-08 10:53:05 -0700

    Add sad path tests for invalid location or food types

--- spec/requests/api/v1/munchies_request_spec.rb
+++ spec/requests/api/v1/munchies_request_spec.rb
@@ -150,6 +150,60 @@ RSpec.describe 'api/v1/munchies api endpoints', :vcr, type: :request do
         expect(data[:type]).to eq('error')
         expect(data[:message]).to eq('Required parameter is missing')
       end
+
+      xit 'will return an error if the location does not exist' do
+        headers = {
+          'CONTENT_TYPE' => 'application/json',
+          'ACCEPT' => 'application/json'
+        }
+        params = {
+          api_key: '35f291ad58d0ac2c5a6275cbe56eb4d3'
+        }
+        user = User.create!(email: 'yogi@picnics.gov', password: 'noturaveragebear')
+        user.api_keys.create!(token: '35f291ad58d0ac2c5a6275cbe56eb4d3')
+
+        get '/api/v1/munchies?location=denv,cq&food=chinese', headers: headers, params: params
+
+        expect(response).to have_http_status(400)
+
+        body = JSON.parse(response.body, symbolize_names: true)
+        expect(body.keys).to include(:data)
+
+        data = body[:data]
+        expect(data.keys).to include(:id, :type, :message)
+        expect(data.keys.count).to eq(3)
+        expect(data[:id]).to eq(nil)
+        expect(data[:type]).to eq('error')
+        # need to update the expected error response
+        expect(data[:message]).to eq('Required parameter is missing')
+      end
+
+      xit 'will return an error if the food type does not exist' do
+        headers = {
+          'CONTENT_TYPE' => 'application/json',
+          'ACCEPT' => 'application/json'
+        }
+        params = {
+          api_key: '35f291ad58d0ac2c5a6275cbe56eb4d3'
+        }
+        user = User.create!(email: 'yogi@picnics.gov', password: 'noturaveragebear')
+        user.api_keys.create!(token: '35f291ad58d0ac2c5a6275cbe56eb4d3')
+
+        get '/api/v1/munchies?location=denver,co&food=hardware', headers: headers, params: params
+
+        expect(response).to have_http_status(400)
+
+        body = JSON.parse(response.body, symbolize_names: true)
+        expect(body.keys).to include(:data)
+
+        data = body[:data]
+        expect(data.keys).to include(:id, :type, :message)
+        expect(data.keys.count).to eq(3)
+        expect(data[:id]).to eq(nil)
+        expect(data[:type]).to eq('error')
+        # need to update the expected error response
+        expect(data[:message]).to eq('Required parameter is missing')
+      end
     end
   end
 end
